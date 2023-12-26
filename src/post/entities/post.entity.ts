ae407ff7d3dff3653e08fdb177f4b3c58af250ff
Gustavo <gustavohdlima@hotmail.com>
2023-12-26 12:47:56 -0300

    Fix: Corrindo anotações erradas na entidade de Post

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,16 +1,16 @@
 import { IsNotEmpty } from "class-validator";
 import { Comment } from "src/comment/entities/comment.entity";
 import { User } from "src/user/entities/user.entity";
-import { ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 export class Post {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @IsNotEmpty()
+  @Column({nullable: false, length: 30})
   title: string
   
-  @IsNotEmpty()
+  @Column({nullable: false, length: 1000})
   content: string
 
   @ManyToOne(() => User, (user) => user.posts, {cascade: true})
